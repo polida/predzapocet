d1e837dd02cadabee309b1850922e0fcd502321d
polida <63658656+polida@users.noreply.github.com>
2022-09-21 15:49:56 +0200

    Add files via upload

---
+++ program/ODP/ODP/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ODP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string StringInput1, StringInput2;
+            int age, count;
+            var candidates = new List<Candidate>();
+            var parties = new List<Party>();
+            Console.WriteLine("zadejte počet kandidátů v obvodu: ");
+            while (!int.TryParse(Console.ReadLine(), out count) || (count < 6 || count > 20))
+                Console.WriteLine("Zadaný počet není platný, opakujte");
+            for (int i = 0; true; i++)
+            {
+                Console.WriteLine("Název strany: ");
+                StringInput1 = Console.ReadLine();
+                Console.WriteLine("Jméno předsedy strany: ");
+                StringInput2 = Console.ReadLine();
+                parties.Add(new Party(StringInput1, StringInput2));
+
+                for (int j = 0; j < count; j++)
+                {
+                    Console.WriteLine("jmeno kandidata:");
+                    StringInput1 = Console.ReadLine();
+                    Console.WriteLine("vek kandidata:");
+                    while (!int.TryParse(Console.ReadLine(), out age) || (age < 18 || age > 120))
+                        Console.WriteLine("Zdaný věk není platný, opakujte");
+                    candidates.Add(new Candidate(StringInput1, age, parties[i]));
+                }
+                Console.WriteLine("název strany: " + parties[i].name);
+                Console.WriteLine("Pokud chceete název strany upravit stiskněte y");
+                if (Console.ReadLine().ToLower() == "y")
+                    parties[i].name = Console.ReadLine();
+                Console.WriteLine("Pokud nechcete zadat další kandidátku stiněte n");
+                if (Console.ReadLine().ToLower() == "n")
+                    break;
+            }
+            Console.WriteLine("Výčet kanditátu podle navrhujícíh stran");
+            foreach (var item in candidates.OrderBy(x => x.party.name))
+                Console.WriteLine(item);
+        }
+    }
+    class Party
+    {
+        public string name;
+        public string president;
+        public Party(string name, string president)
+        {
+            this.name = name;
+            this.president = president;
+        }
+    }
+    class Candidate
+    {
+        private string name;
+        private int age;
+        public Party party;
+        public Candidate(string name, int age, Party party)
+        {
+            this.name = name;
+            this.age = age;
+            this.party = party;
+        }
+        public override string ToString()
+        {
+            return "Kandidát: " + name + " věk: " + age + " navrhující strana: " + party.name + "(předseda: " + party.president + ")";
+        }
+    }
+}
+
---
+++ program/array_algoritm/array_algoritm/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace array_algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var array1 = new int[5];
+            string input;
+            int search, x, y;
+            for (int i = 0; i < array1.Length; i++)
+            {
+                array1[i] = rand.Next(20);
+                Console.Write(array1[i] + " ");
+            }
+            Console.WriteLine();
+            do
+            {
+                Console.WriteLine("1-Buble Sort\n2-Search\n3-Exit");
+                input = Console.ReadLine();
+                if (input == "1")
+                    ArrayFunctions.BubleSort(array1);
+                else if (input == "2")
+                {
+                    Console.Write("prvek k vyhledání: ");
+                    while (!int.TryParse(Console.ReadLine(), out search))
+                        Console.WriteLine("Neplatné číslo, zadejte znovu:");
+                    Console.WriteLine("prvek se nachází na této pozici:" + ArrayFunctions.LinearSearch(array1, search) + "\n");
+                }
+                else if (input == "3")
+                    break;
+                else
+                    Console.WriteLine("Neplatná volba\n");
+            } while (true);
+            ///2D POLE
+            do
+            {
+                Console.Write("Zadejte rozměr x pole: ");
+                x = Extensions.IntInput();
+                Console.Write("Zadejte rozměr y pole: ");
+                y = Extensions.IntInput();
+                var array2 = new int[x, y];
+                ArrayFunctions.Print2D(array2);
+                Console.Write("Zadejte požadavanou sořadnici x pro vložení čísla: ");
+                x = Extensions.IntInput(); ;
+                Console.Write("Zadejte požadavanou sořadnici y pro vložení čísla: ");
+                y = Extensions.IntInput();
+                Console.Write("Zadejte požadavanou požadavanou hodnotu: ");
+                array2[x, y] = Extensions.IntInput();
+                ArrayFunctions.Print2D(array2);
+                Console.WriteLine("Pro ukončení programu zadejte konec");
+                if (Console.ReadLine().ToLower() == "konec")
+                    break;
+            } while (true);
+
+        }
+        class ArrayFunctions
+        {
+            public static int[] BubleSort(int[] array)
+            {
+                int temp = 0;
+                for (int i = 0; i < array.Length; i++)
+                    for (int j = 0; j < array.Length - 1; j++)
+                        if (array[j] > array[j + 1])
+                        {
+                            temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                Print(array);
+                return array;
+            }
+            public static int LinearSearch(int[] array, int searching)
+            {
+                for (int i = 0; i < array.Length; i++)
+                    if (array[i] == searching)
+                    {
+                        return i;
+                    }
+                return -1;
+            }
+            private static void Print(int[] array)
+            {
+                foreach (int item in array)
+                    Console.Write(item + " ");
+                Console.WriteLine();
+            }
+            public static void Print2D(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                        Console.Write(array[i, j] + " ");
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+        }
+        class Extensions
+        {
+            public static int IntInput()
+            {
+                int input;
+                while (!int.TryParse(Console.ReadLine(), out input))
+                    Console.WriteLine("Error");
+                return input;
+            }
+        }
+    }
+}
+
